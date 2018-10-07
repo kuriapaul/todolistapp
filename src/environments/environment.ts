4b71e00acaf4de2a629bfa488a2ef14aa5965e08
kuriapaul <pauwains@gmail.com>
2018-10-07 15:25:12 +0300

    added firebase to the app

---
+++ src/environments/environment.ts
@@ -0,0 +1,24 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebase : {
+    apiKey: "AIzaSyDpcFq38G2K1g1u9i3PnXy9_65shSd73nQ",
+    authDomain: "todolistapp-ef5ce.firebaseapp.com",
+    databaseURL: "https://todolistapp-ef5ce.firebaseio.com",
+    projectId: "todolistapp-ef5ce",
+    storageBucket: "todolistapp-ef5ce.appspot.com",
+    messagingSenderId: "553151487213"
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
